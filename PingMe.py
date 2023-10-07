1ad156a1dad63f1fd89348498e2c9e0564f707f0
Unzajebable <maciejankowskibiznes@gmail.com>
2023-10-07 14:59:59 +0200

    script for creating a database and tables added

--- PingMe.py
+++ PingMe.py
@@ -1,7 +1,6 @@
 import argparse
 import hashlib
-from .helpers import create_db as cdb
+from helpers import create_db as cdb
 
 
 cdb.create_db()
-
--- helpers/create_db.py
+++ helpers/create_db.py
@@ -1,16 +1,45 @@
-from psycopg2 import connect
-#from psycopg2.extras import RealDictCursor
+from psycopg2 import connect, OperationalError, errorcodes, errors
 
 
 def create_db():
     connection = connect(host='localhost', user='postgres', password='2121', database='test')
-    #cursor = connection.cursor(cursor_factory=RealDictCursor)
+    connection.autocommit = True
     cursor = connection.cursor()
 
-    sql1 = 'CREATE DATABASE PingMe_db;'
+    create_all = True
+    sql1 = 'CREATE DATABASE ping_me_db;'
 
-    cursor.execute(sql1)
+    try:
+        cursor.execute(sql1)
+    except Exception as err:
+        create_all = False
+        print('FYI:', err)
+        #print("Exception type:", type(err))
+
+    if create_all:
+        cursor.close()
+        connection.close() #connection closed so we need another autocommit = true
+        connection = connect(host='localhost', user='postgres', password='2121', database='ping_me_db')
+        connection.autocommit = True
+
+        cursor = connection.cursor()
+        sql2 = """CREATE TABLE users(
+        id serial PRIMARY KEY,
+        username varchar(32),
+        hashed_password varchar(80));"""
+        cursor.execute(sql2)
+        cursor.close()
+
+        cursor = connection.cursor()
+        sql3 = """CREATE TABLE messages(
+        id serial PRIMARY KEY,
+        from_id int NOT NULL REFERENCES users(id),
+        to_id int NOT NULL REFERENCES users(id),
+        created_date timestamp default current_timestamp,
+        message text);"""
+        cursor.execute(sql3)
 
     cursor.close()
     connection.close()
+    print("database and tables created and fully operational")
 
